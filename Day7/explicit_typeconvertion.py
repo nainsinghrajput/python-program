15b11e3f7198bf50d23063e97ffb292eb9731bc1
nainsinghrajput <rajputajnna@gmail.com>
2026-04-30 14:51:17 +0530

    implicit type convertion

--- Day7/explicit_typeconvertion.py
+++ Day7/explicit_typeconvertion.py
@@ -6,6 +6,14 @@ b = "12"
 b = int(b)
 print(type(b)) 
 
-c = "Nain Singh"
-c = int(c) # integer is invalid to this string input 
-print(type(c))
+ c = "Nain Singh"
+ c = int(c) # integer is invalid to this string input 
+ print(type(c))
+
+# boolean true false program
+
+d = 12
+print(bool(d)) # True 
+
+e = 0  # false { 7 types :- 0 , 0.0 , 0j(it is complex), " ", [] , () , {} , }
+print(bool(e))
